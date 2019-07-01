fd5158da51002fc829b06ad09e0c14a83aee2183
Rithie <euler.sistemas@gmail.com>
2019-07-01 16:57:14 -0300

    minor changes

--- src/containers/LogIn/index.jsx
+++ src/containers/LogIn/index.jsx
@@ -10,8 +10,8 @@ const LogIn = () => (
       <div className="account__card">
         <div className="account__head">
           <h3 className="account__title">Welcome to
-            <span className="account__logo"> Easy
-              <span className="account__logo-accent">DEV</span>
+            <span className="account__logo"> Trocka
+              <span className="account__logo-accent">App</span>
             </span>
           </h3>
           <h4 className="account__subhead subhead">Start your business easily</h4>
